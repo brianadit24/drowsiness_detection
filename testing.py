3221f67141a678995f8fc780aad56efdf2b447ca
muktiarafi <muktiarafi@gmail.com>
2021-04-16 23:14:16 +0700

    play Jojo's Bizarre Adventure- Awaken(Pillar Men Theme) when eyes is closed

--- testing.py
+++ testing.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+import vlc
 
 model = tf.keras.models.load_model("model/drowsiness_full.h5")
 path = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + path)
+awake = vlc.MediaPlayer(
+    "assets/Jojo's Bizarre Adventure- Awaken(Pillar Men Theme).mp3")
 
 cap = cv2.VideoCapture(1)
 # Check Webcam can open correctly
@@ -15,10 +18,11 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
-    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+    eye_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_eye.xml")
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     eyes = eye_cascade.detectMultiScale(gray, 1.1, 4)
-    for (x,y,w,h) in eyes:
+    for (x, y, w, h) in eyes:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyess = eye_cascade.detectMultiScale(roi_gray)
@@ -28,32 +32,34 @@ while True:
             for (ex, ey, ew, eh) in eyess:
                 eyes_roi = roi_color[ey: ey+eh, ex:ex+ew]
     final_image = cv2.resize(eyes_roi, (224, 224))
-    final_image = np.expand_dims(final_image, axis=0) ## Butuh 4 Dimensi
-    final_image=final_image/255.0
+    final_image = np.expand_dims(final_image, axis=0)  # Butuh 4 Dimensi
+    final_image = final_image/255.0
 
     Predictions = model.predict(final_image)
-    if (Predictions>0.5):
+    if (Predictions > 0.5):
         status = "Open Eyes"
+        awake.stop()
     else:
         status = "Closed Eyes"
+        awake.play()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     print(faceCascade.empty())
-    faces = faceCascade.detectMultiScale(gray,1.1,4)
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
 
     # Draw Rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    
+
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     # Use putText() method for
     # Inserting text on video
     cv2.putText(frame,
                 status,
-                (50,50),
+                (50, 50),
                 font, 3,
-                (0,0,255), 2,
+                (0, 0, 255), 2,
                 cv2.LINE_4)
     cv2.imshow('Drowsiness Detection', frame)
 
